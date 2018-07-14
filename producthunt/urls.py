4f9bb8c13b818278df22d2ae73d5baf09a0b680b
Duskhorizon <duskhorizon@home-desktop.home>
2018-07-14 11:48:12 +0200

    Added Iconic support

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 import products.views
 
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', products.views.home, name='home'),
     path('accounts/', include('accounts.urls')),
-]
+    path('products/', include('products.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
